4b9e2568347403a342d9c9c98c41780ca0e32f12
norgeous <norgeous@users.noreply.github.com>
2025-06-17 00:52:36 +0100

    Update index.tsx

--- components/GradientRating/index.tsx
+++ components/GradientRating/index.tsx
@@ -1,5 +1,24 @@
-const GradientRating = () => {
-    return 'hello'
+'use client';
+
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+
+const Piece = styled.div`
+  width: 100%;
+  background: pink;
+  height: 40px;
+`;
+
+const GradientRating = ({ max = 10 }) => {
+  return (
+    <Wrap>
+      {Array.from({ length: max },(_,k) => <Piece key={k} max={max} />)}
+    </Wrap>
+  );
 };
 
 export default GradientRating;
