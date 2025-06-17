3009ddd535afbcbe74cb5349cd845a0e8ab78857
norgeous <norgeous@users.noreply.github.com>
2025-06-17 21:06:15 +0100

    thinking about the top level strategy selector

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
 import Fund from '@/components/Fund';
 import cautiousMockData from '@/mocks/Cautious.json';
 
+const investmentStrategies = [
+  {
+    name: 'Growth Funds Variations',
+    fundOptions: [
+      { name: 'Cautious', endpointId: 'BYW8RV9' },
+      { name: 'Balanced', endpointId: 'BYW8RX1' },
+      { name: 'Adventurous', endpointId: 'BYW8VG2' },
+    ],
+  },
+  {
+    name: 'Responsible Growth Fund',
+    fundOptions: [{ name: 'Responsible', endpointId: 'BN0S2V9' }],
+  },
+];
+
 const Home = () => {
-  return <Fund data={cautiousMockData} />;
+  return (
+    <>
+      <pre>{JSON.stringify(investmentStrategies, null, 2)}</pre>
+      <Fund data={cautiousMockData} />
+    </>
+  );
 };
 
 export default Home;
