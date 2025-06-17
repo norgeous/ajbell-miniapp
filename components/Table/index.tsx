a48e1deb641dbdfa78d075db5d5d8b46308dfddd
norgeous <norgeous@users.noreply.github.com>
2025-06-17 23:48:31 +0100

    Update index.tsx

--- components/Table/index.tsx
+++ components/Table/index.tsx
@@ -9,14 +9,16 @@ const adapt = (data: object[]) => data.map(item => Object.values(item));
 
 const DataTable = ({ headings, data }: IDataTable) => (
   <Table>
-    <Tr>{headings?.map(heading => <th key={heading}>{heading}</th>)}</Tr>
-    {adapt(data).map((row, indexRow) => (
-      <Tr key={indexRow}>
-        {row.map((cell, indexCell) => (
-          <Td key={indexCell}>{cell}</Td>
-        ))}
-      </Tr>
-    ))}
+    <tbody>
+      <Tr>{headings?.map(heading => <th key={heading}>{heading}</th>)}</Tr>
+      {adapt(data).map((row, indexRow) => (
+        <Tr key={indexRow}>
+          {row.map((cell, indexCell) => (
+            <Td key={indexCell}>{cell}</Td>
+          ))}
+        </Tr>
+      ))}
+    </tbody>
   </Table>
 );
 
