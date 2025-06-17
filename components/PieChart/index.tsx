a3abbb0d9751c4e9c577553eae5815f090c70236
norgeous <norgeous@users.noreply.github.com>
2025-06-17 12:58:05 +0100

    Update index.tsx

--- components/PieChart/index.tsx
+++ components/PieChart/index.tsx
@@ -7,7 +7,7 @@ interface IPieChart {
 }
 
 const PieChart = ({ values = demo }:IPieChart) => (
-  <svg width="100" height="100">
+  <svg width="100" height="100" viewBox="0 0 100 100">
     {adaptPieValues(values).map(({ label, value, offsetDeg }, index) => (
       <PieSlice key={label} index={index} value={value} offsetDeg={offsetDeg} />
     ))}
