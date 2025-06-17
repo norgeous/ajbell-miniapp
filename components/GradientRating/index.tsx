1f5f3da0f88506ed6f384ebfa89b527ab5b703c0
norgeous <norgeous@users.noreply.github.com>
2025-06-17 16:36:23 +0100

    Update index.tsx

--- components/GradientRating/index.tsx
+++ components/GradientRating/index.tsx
@@ -1,4 +1,4 @@
-import { Wrap, Piece } from './styled';
+import { Wrap, Piece } from "./styled";
 
 interface IGradientRating {
   value: number;
@@ -9,7 +9,7 @@ const GradientRating = ({ value, max = 10 }: IGradientRating) => {
   return (
     <Wrap>
       {Array.from({ length: max }, (_, k) => (
-        <Piece key={k} index={k} max={max} selected={value === k + 1}/>
+        <Piece key={k} index={k} max={max} selected={value === k + 1} />
       ))}
     </Wrap>
   );
