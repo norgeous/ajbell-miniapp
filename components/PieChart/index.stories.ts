2fca44c20ab98373b093a0f3a8ae65211e48598e
norgeous <norgeous@users.noreply.github.com>
2025-06-17 13:16:36 +0100

    lint

--- components/PieChart/index.stories.ts
+++ components/PieChart/index.stories.ts
@@ -10,43 +10,43 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const halfs: Story = {
+export const Halfs: Story = {
   args: {
     values: [{label:'one',value:50},{label:'two',value:50}],
   },
 };
 
-export const thirds: Story = {
+export const Thirds: Story = {
   args: {
     values: [{label:'one',value:33.33333},{label:'two',value:33.33333},{label:'thr',value:33.33333}],
   },
 };
 
-export const quarters: Story = {
+export const Quarters: Story = {
   args: {
     values: [{label:'one',value:25},{label:'two',value:25},{label:'thr',value:25},{label:'fou',value:25}],
   },
 };
 
-export const fifths: Story = {
+export const Fifths: Story = {
   args: {
     values: [{label:'one',value:20},{label:'two',value:20},{label:'thr',value:20},{label:'fou',value:20},{label:'fiv',value:20}],
   },
 };
 
-export const sixths: Story = {
+export const Sixths: Story = {
   args: {
     values: [{label:'one',value:100/6},{label:'two',value:100/6},{label:'thr',value:100/6},{label:'fou',value:100/6},{label:'fiv',value:100/6},{label:'six',value:100/6}],
   },
 };
 
-export const sevenths: Story = {
+export const Sevenths: Story = {
   args: {
     values: [{label:'one',value:100/7},{label:'two',value:100/7},{label:'thr',value:100/7},{label:'fou',value:100/7},{label:'fiv',value:100/7},{label:'six',value:100/7},{label:'sev',value:100/7}],
   },
 };
 
-export const eighths: Story = {
+export const Eighths: Story = {
   args: {
     values: [{label:'one',value:100/8},{label:'two',value:100/8},{label:'thr',value:100/8},{label:'fou',value:100/8},{label:'fiv',value:100/8},{label:'six',value:100/8},{label:'sev',value:100/8},{label:'eig',value:100/8}],
   },
