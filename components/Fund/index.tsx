46be3893ddec4180a5b720653fac582ef1fe9428
norgeous <norgeous@users.noreply.github.com>
2025-06-17 18:44:27 +0100

    Update index.tsx

--- components/Fund/index.tsx
+++ components/Fund/index.tsx
@@ -3,10 +3,11 @@ import PieChart from '../PieChart';
 import StarRating from '../StarRating';
 import DataTable from '../Table';
 
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
 const ExternalLink = ({ href, children }) => (
   <a href={href} target="_blank" style={{ display: 'block' }}>
-    {children}
-    ➡️
+    {children} <FaExternalLinkAlt />
   </a>
 );
 
@@ -18,6 +19,7 @@ const Fund = ({ data }: IFund) => {
   return (
     <>
       <h1>{data.data.quote.name}</h1>
+      <div>{data.data.quote.sectorName}</div>
       <p>{data.data.profile.objective}</p>
       <StarRating value={data.data.ratings.analystRating} />
       <GradientRating value={data.data.ratings.SRRI} />
@@ -30,7 +32,6 @@ const Fund = ({ data }: IFund) => {
         {data.data.quote.currency}
         {data.data.quote.ongoingCharge}
       </div>
-      <div>{data.data.quote.sectorName}</div>
       <PieChart values={data.data.portfolio.asset} />
       {data.data.documents.map(({ id, type, url }) => (
         <ExternalLink key={id} href={url}>
