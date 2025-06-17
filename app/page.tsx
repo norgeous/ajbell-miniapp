903488e45d55ce3bee4e1f1bd2584b5934c6640d
norgeous <norgeous@users.noreply.github.com>
2025-06-17 12:19:28 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import StarRating from "@/components/StarRating";
 import GradientRating from "@/components/GradientRating";
+import PieChart from "@/components/PieChart";
 
 export default function Home() {
   return (
@@ -16,48 +17,10 @@ export default function Home() {
         <GradientRating value={3} max={5} />
         <GradientRating value={7} />
         <GradientRating value={17} max={20} />
-        {/*
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <PieChart />
+        <PieChart values={[{label:'one',value:50},{label:'two',value:50}]}/>
+        <PieChart values={[{label:'one',value:33.33333},{label:'two',value:33.33333},{label:'thr',value:33.33333}]}/>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-        */}
 
       </main>
       <footer className={styles.footer}>
