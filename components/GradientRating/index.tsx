b6a76f547eb1e01d49ddd67ce9568a6856e3dbbf
norgeous <norgeous@users.noreply.github.com>
2025-06-17 10:03:53 +0100

    separate styled client components in GradientRating

--- components/GradientRating/index.tsx
+++ components/GradientRating/index.tsx
@@ -1,29 +1,15 @@
-'use client';
+import { Wrap, Piece } from './styled';
 
-import styled, { css } from 'styled-components';
+interface IGradientRating {
+  value: number;
+  max?: number;
+}
 
-const Wrap = styled.div`
-  display: flex;
-  gap: 2px;
-  min-width: 200px;
-`;
-
-const Piece = styled.span`
-  width: 100%;
-  height: 20px;
-  background-image: linear-gradient(to right, green, orange, red);
-  background-size: ${({ max }) => max * 100}%;
-  background-position: ${({ index, max }) => (100 / max) * index}%;
-  ${({ selected }) => selected && css`
-    background: lightblue;
-  `}
-`;
-
-const GradientRating = ({ max = 10, value }) => {
+const GradientRating = ({ value, max = 10 }: IGradientRating) => {
   return (
     <Wrap>
       {Array.from({ length: max }, (_, k) => (
-        <Piece key={k} index={k} max={max} selected={value === k+1}/>
+        <Piece key={k} index={k} max={max} selected={value === k + 1}/>
       ))}
     </Wrap>
   );
---
+++ components/GradientRating/styled.ts
@@ -0,0 +1,26 @@
+'use client';
+
+import styled, { css } from 'styled-components';
+
+export const Wrap = styled.div`
+  display: flex;
+  gap: 2px;
+  min-width: 200px;
+`;
+
+interface IPiece {
+  index: number;
+  max: number;
+  selected: boolean;
+}
+
+export const Piece = styled.span<IPiece>`
+  width: 100%;
+  height: 20px;
+  background-image: linear-gradient(to right, green, orange, red);
+  background-size: ${({ max }) => max * 100}%;
+  background-position: ${({ index, max }) => (100 / max) * index}%;
+  ${({ selected }) => selected && css`
+    background: lightblue;
+  `}
+`;
