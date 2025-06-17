d6ba7471cce6590a893b33bef3b9a5b964dbf71f
norgeous <norgeous@users.noreply.github.com>
2025-06-17 17:54:25 +0100

    tidy

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import styles from './page.module.css';
 import Fund from '@/components/Fund';
 import cautiousMockData from '@/mocks/Cautious.json';
 
 const Home = () => {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Fund data={cautiousMockData} />
-      </main>
-      <footer className={styles.footer}></footer>
-    </div>
-  );
+  return <Fund data={cautiousMockData} />;
 };
 
 export default Home;
