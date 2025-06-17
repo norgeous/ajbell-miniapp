22c2fe0832d1bf8aa173771a9addc8de9048ffbf
norgeous <norgeous@users.noreply.github.com>
2025-06-17 18:14:27 +0100

    Update index.tsx

--- components/Fund/index.tsx
+++ components/Fund/index.tsx
@@ -2,6 +2,15 @@ import GradientRating from '../GradientRating';
 import PieChart from '../PieChart';
 import StarRating from '../StarRating';
 
+const ExternalLink = ({ href, children }) => (
+  <a href={href} target="_blank" style={{ display: 'block' }}>
+    {children}
+    ➡️
+  </a>
+);
+
+const Table = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+
 interface IFund {
   data: any;
 }
@@ -13,9 +22,19 @@ const Fund = ({ data }: IFund) => {
       <p>{data.data.profile.objective}</p>
       <StarRating value={data.data.ratings.analystRating} />
       <GradientRating value={data.data.ratings.SRRI} />
+      <div>{data.data.ratings.analystRatingLabel}</div>
       <PieChart values={data.data.portfolio.asset} />
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {data.data.documents.map(({ id, type, url }) => (
+        <ExternalLink key={id} href={url}>
+          {type}
+        </ExternalLink>
+      ))}
+      <Table data={data.data.portfolio.top10Holdings} />
+      <div>£{data.data.quote.lastPrice}</div>
+      <div>£{data.data.quote.ongoingCharge}</div>
+      <div>{data.data.quote.sectorName}</div>
+      {/* <hr /> */}
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
     </>
   );
 };
