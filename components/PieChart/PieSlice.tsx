bce4adef98de521284aab1a5361bbe8840dc7ef1
norgeous <norgeous@users.noreply.github.com>
2025-06-17 13:37:07 +0100

    use styled components for PieSlice

--- components/PieChart/PieSlice.tsx
+++ components/PieChart/PieSlice.tsx
@@ -1,33 +1,13 @@
-const circumference = (Math.PI * 2) * 25;
+import { ISlice, Slice } from './styled';
 
-interface IPieSlice {
-  index: number; // used to choose colour
-  value: number; // size in percent of slice
-  offsetDeg: number; // degrees of rotation of this slice
-}
-
-const sliceColors = [
-  'oklch(.66 .15 0)',
-  'oklch(.66 .15 90)',
-  'oklch(.66 .15 180)',
-  'oklch(.66 .15 270)',
-
-  'oklch(.66 .15 45)',
-  'oklch(.66 .15 135)',
-  'oklch(.66 .15 225)',
-  'oklch(.66 .15 315)',
-];
-
-const PieSlice = ({ index, value, offsetDeg }:IPieSlice) => (
-  <circle
+const PieSlice = ({ index, value, offsetDeg }:ISlice) => (
+  <Slice
+    index={index}
+    value={value}
+    offsetDeg={offsetDeg}
     r="25"
     cx="50"
     cy="50"
-    fill="transparent"
-    transform={`rotate(${-90 + offsetDeg} 50 50)`}
-    stroke={sliceColors[index] || 'red'}
-    strokeWidth="50"
-    strokeDasharray={`${((value * circumference) / 100)} ${circumference}`}
   />
 );
 
--- components/PieChart/styled.ts
+++ components/PieChart/styled.ts
@@ -2,9 +2,39 @@
 
 import styled from 'styled-components';
 
+const sliceColors = [
+  'oklch(.66 .15 0)',
+  'oklch(.66 .15 90)',
+  'oklch(.66 .15 180)',
+  'oklch(.66 .15 270)',
+
+  'oklch(.66 .15 45)',
+  'oklch(.66 .15 135)',
+  'oklch(.66 .15 225)',
+  'oklch(.66 .15 315)',
+];
+
 export const Svg = styled.svg`
   display: block;
   width: 100%;
   height: 100%;
   max-width: 500px;
+`;
+
+
+const circumference = (Math.PI * 2) * 25;
+
+interface ISlice {
+  index: number;
+  value: number; // size in percent of slice
+  offsetDeg: number; // degrees of rotation of this slice
+}
+
+export const Slice = styled.circle<ISlice>`
+  fill: transparent;
+  stroke: ${({ index }) => sliceColors[index] || 'red'};
+  stroke-width: 50px;
+  transform: ${({ offsetDeg }) => `rotate(${-90 + offsetDeg}deg)`};
+  transform-origin: 50% 50%;
+  stroke-dasharray: ${({value})=>`${((value * circumference) / 100)} ${circumference}`};
 `;
