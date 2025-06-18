b12022dcd8a8f2dbce7aa459d60e499c435f07dd
norgeous <norgeous@users.noreply.github.com>
2025-06-18 11:00:03 +0100

    Update index.stories.ts

--- components/Button/index.stories.ts
+++ components/Button/index.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { fn } from 'storybook/test';
 import Button from '.';
 
 const meta = {
@@ -14,5 +15,6 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     children: 'hello',
+    onClick: fn(),
   },
 };
