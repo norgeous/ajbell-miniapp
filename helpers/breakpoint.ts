4b90b0dbd662881a14ead96143f1bedac1a312a4
norgeous <norgeous@users.noreply.github.com>
2025-06-19 23:17:13 +0100

    Update breakpoint.ts

--- helpers/breakpoint.ts
+++ helpers/breakpoint.ts
@@ -1,7 +1,7 @@
 import { css, RuleSet } from 'styled-components';
 
 export const desktopUp = (rules: RuleSet) => css`
-  @media (min-width: 768px) {
+  @media (min-width: 820px) {
     ${rules}
   }
 `;
