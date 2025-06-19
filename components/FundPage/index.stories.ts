ef29d8786fbe26f5097540940974c75c6e0549f8
norgeous <norgeous@users.noreply.github.com>
2025-06-19 22:38:58 +0100

    FundPage story

---
+++ components/FundPage/index.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { AJBellResponseType } from '@/helpers/endpoint';
+import FundPage from '.';
+import cautiousMockData from '@/mocks/Cautious.json';
+import balancedMockData from '@/mocks/Balanced.json';
+import adventurousMockData from '@/mocks/Adventurous.json';
+import responsibleMockData from '@/mocks/Responsible.json';
+
+const meta = {
+  title: 'Components/FundPage',
+  component: FundPage,
+  tags: ['autodocs'],
+} satisfies Meta<typeof FundPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    allData: {
+      BYW8RV9: cautiousMockData as AJBellResponseType,
+      BYW8RX1: balancedMockData as AJBellResponseType,
+      BYW8VG2: adventurousMockData as AJBellResponseType,
+      BN0S2V9: responsibleMockData as AJBellResponseType,
+    },
+  },
+};
