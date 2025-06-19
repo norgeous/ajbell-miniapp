1f68425f3853d733c6d4c0b859060cc6959674fb
norgeous <norgeous@users.noreply.github.com>
2025-06-19 23:17:50 +0100

    Revert "Update breakpoint.ts"
    This reverts commit 4b90b0dbd662881a14ead96143f1bedac1a312a4.

--- helpers/breakpoint.ts
+++ helpers/breakpoint.ts
@@ -1,7 +1,7 @@
 import { css, RuleSet } from 'styled-components';
 
 export const desktopUp = (rules: RuleSet) => css`
-  @media (min-width: 820px) {
+  @media (min-width: 768px) {
     ${rules}
   }
 `;
