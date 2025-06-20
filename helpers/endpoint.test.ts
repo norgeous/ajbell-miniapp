233c52cc5e08028b66301d0fca8184d49e604ef9
norgeous <norgeous@users.noreply.github.com>
2025-06-20 12:36:17 +0100

    Update endpoint.test.ts

--- helpers/endpoint.test.ts
+++ helpers/endpoint.test.ts
@@ -1,7 +1,8 @@
 import { expect, test } from 'vitest';
 import { fetchAll } from './endpoint';
 
-test('adds 1 + 2 to equal 3', () => {
+test('adds 1 + 2 to equal 3', async () => {
   console.log('VITEST!');
+  const data = await fetchAll;
   expect(3).toBe(3);
 });
